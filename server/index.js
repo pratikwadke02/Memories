844259123536eaf3612f65b06388f2d38394877e
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2022-06-21 02:15:21 +0530

    intial commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+
+const username = "pratikwadke"
+const password = "pratik02"
+const CONNECTION_URL = `mongodb+srv://${username}:${password}@cluster0.xkop1sg.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> app.listen(PORT, ()=> console.log(`Server running on Port: ${PORT}`)))
+.catch((error)=> console.log(`${error} did not connect`));
+
+// mongoose.set('useFindAndModify', false);
