e5e46509bd399a75879f156c50d79fe089f90c99
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2022-06-21 11:14:32 +0530

    router works

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+app.use('/posts', postRoutes);
+//localhost:5000/posts
 
 const username = "pratikwadke"
 const password = "pratik02"
